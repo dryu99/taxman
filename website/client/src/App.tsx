206b6635234ffadbc0d13e613273027e8abb9ccf
Daniel Ryu <rujjiryu99@gmail.com>
2021-07-25 20:51:09 -0700

    setup website style packages and boilerplate

--- website/client/src/App.tsx
+++ website/client/src/App.tsx
@@ -1,25 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import GlobalStyle from "./styles/global-style";
+import ThemeProviderWrapper from "./styles/theme-provider";
+
+const PageContainer = styled.div`
+  padding: 5em 10vw;
+`;
 
 function App() {
+  const [username, setUsername] = useState("");
+
+  useEffect(() => {
+    const fragment = new URLSearchParams(window.location.hash.slice(1));
+    const [accessToken, tokenType] = [
+      fragment.get("access_token"),
+      fragment.get("token_type"),
+    ];
+
+    fetch("https://discord.com/api/users/@me", {
+      headers: {
+        authorization: `${tokenType} ${accessToken}`,
+      },
+    })
+      .then((result) => result.json())
+      .then((res) => {
+        console.log("hi", res);
+        const { username } = res;
+        setUsername(username);
+      })
+      .catch((error) => {
+        console.log("You might have ad block on"); // TODO should display this msg in ui
+        console.error(error);
+      });
+  }, []);
+
+  const showName = username.length > 0;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProviderWrapper>
+      <GlobalStyle />
+      <PageContainer>
+        <div>
+          <div>Hoi!</div>
+          {showName ? (
+            <p>{username}</p>
+          ) : (
+            <a
+              id="login"
+              href="https://discord.com/api/oauth2/authorize?client_id=861845717613150258&redirect_uri=http%3A%2F%2Flocalhost%3A3001%2F&response_type=token&scope=identify"
+            >
+              Identify Yourself
+            </a>
+          )}
+        </div>
+      </PageContainer>
+    </ThemeProviderWrapper>
   );
 }
 
