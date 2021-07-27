7b38f4ee478e407e667eab68a38107c2d2f95fb1
Daniel Ryu <rujjiryu99@gmail.com>
2021-07-27 00:29:31 -0700

    refactor dashboard token retrieval logic

--- website/client/src/pages/dashboard.tsx
+++ website/client/src/pages/dashboard.tsx
@@ -1,46 +1,54 @@
 import React, { useEffect, useState } from "react";
 
+interface TokenData {
+  accessToken: string;
+  tokenType: string;
+}
+
+const TOKEN_DATA_CACHE_KEY = "discord_token_data";
+
+const getTokenData = (): TokenData | undefined => {
+  // check url params
+  const fragment = new URLSearchParams(window.location.hash.slice(1));
+  const accessToken = fragment.get("access_token");
+  const tokenType = fragment.get("token_type");
+  if (accessToken && tokenType) return { accessToken, tokenType };
+
+  // check browser cache
+  const cachedTokenDataJSON = localStorage.getItem(TOKEN_DATA_CACHE_KEY);
+  if (cachedTokenDataJSON) {
+    const cachedTokenData: TokenData = JSON.parse(cachedTokenDataJSON);
+    return { ...cachedTokenData };
+  }
+
+  return undefined;
+};
+
 interface DashboardPageProps {}
 
 // TODO this would be a lot cleaner if we
 //   1. did authorization code grants instead of implicit (have to setup server)
+//        - actually it prob looks the same, just with codes instead of tokens lol
 //   2. used nextjs (maybe)
+//        - could just use loader for now while additional network reqs are being made
 
 const DashboardPage: React.FC<DashboardPageProps> = ({}) => {
   const [token, setToken] = useState("");
   const [username, setUsername] = useState("");
 
   useEffect(() => {
-    const fragment = new URLSearchParams(window.location.hash.slice(1));
-    let [accessToken, tokenType] = [
-      fragment.get("access_token"),
-      fragment.get("token_type"),
-    ];
-
-    // user naved here via discord bot (url has no params)
-    if (!accessToken || !tokenType) {
-      // check for token
-      const tokenDataJSON = localStorage.getItem("DISCORD_TOKEN_DATA");
-      if (tokenDataJSON === null) {
-        // do oauth workflow if token isn't cached
-        window.location.href =
-          "https://discord.com/api/oauth2/authorize?client_id=861845717613150258&redirect_uri=http%3A%2F%2Flocalhost%3A3001%2Fdashboard&response_type=token&scope=identify";
-        return;
-      }
-
-      const tokenData: { accessToken: string; tokenType: string } =
-        JSON.parse(tokenDataJSON);
-      accessToken = tokenData.accessToken;
-      tokenType = tokenData.tokenType;
+    const tokenData = getTokenData();
+    if (!tokenData) {
+      window.location.href =
+        "https://discord.com/api/oauth2/authorize?client_id=861845717613150258&redirect_uri=http%3A%2F%2Flocalhost%3A3001%2Fdashboard&response_type=token&scope=identify";
+      return;
     }
 
-    // user naved here via oauth (or trickled down from above blockimpl)
+    // cache token
+    localStorage.setItem(TOKEN_DATA_CACHE_KEY, JSON.stringify(tokenData));
 
-    // cache token TODO we should only do this if localstorage item is null
-    const newTokenDataJSON = JSON.stringify({ accessToken, tokenType });
-    localStorage.setItem("DISCORD_TOKEN_DATA", newTokenDataJSON);
-
-    // fetch discord data with token
+    // fetch discord data
+    const { accessToken, tokenType } = tokenData;
     fetch("https://discord.com/api/users/@me", {
       headers: {
         authorization: `${tokenType} ${accessToken}`,
