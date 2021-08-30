e0e1e616d6f202060fb28c8cc7232ca37ba95bb0
Daniel Ryu <rujjiryu99@gmail.com>
2021-08-30 01:07:21 -0700

    Edit VPS start script

--- start.sh
+++ start.sh
@@ -1,4 +1,2 @@
 # Production start for all apps (Should only need to be run once)
-
-cd bot
-pm2 start npm --name "discord-bot" -- start
+NODE_ENV=production pm2 start bot/dist/index.js --name "discord-bot"
